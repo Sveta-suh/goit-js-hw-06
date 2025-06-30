842917e026ec398e03bbc9306cd557d0b848333c
SVETLANA SUKHOVA <svetsuh24@gmail.com>
2025-06-30 16:47:26 +0300

    JS

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+class Storage {
+  #items;
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
+  }
+}
